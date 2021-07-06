3a8cd16784ad262e1ebcc00b13527eabc5c13caf
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-07-06 12:41:34 +0530

    init commit

---
+++ src/ServiceCenterExample.java
@@ -0,0 +1,12 @@
+/**
+ * @author Shevan
+ * @created 06/07/2021 - 12:38
+ * @project COVID-19 VACCINATION CENTER PROGRAM
+ * @file ServiceCenterExample
+ */
+public class ServiceCenterExample {
+
+    public static void main(String[] args) {
+
+    }
+}
